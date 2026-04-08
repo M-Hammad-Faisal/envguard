3f8dbcab6dcf66105a05e4e0ec9f2e4eb143f1f7
Muhammad Hammad Faisal <hammadfaisal178@gmail.com>
2026-04-08 15:11:54 +0500

    add version flag

--- cmd/root.go
+++ cmd/root.go
@@ -3,14 +3,14 @@ package cmd
 import (
 	"fmt"
 	"os"
-
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "envguard",
-	Short: "EnvGuard — secure environment variable management for teams",
-	Long:  "EnvGuard encrypts your .env file for team sharing and prevents hardcoded secrets from reaching your Git history.",
+	Use:     "envguard",
+	Version: "0.1.0",
+	Short:   "EnvGuard — secure environment variable management for teams",
+	Long:    "EnvGuard encrypts your .env file for team sharing and prevents hardcoded secrets from reaching your Git history.",
 }
 
 func Execute() {
